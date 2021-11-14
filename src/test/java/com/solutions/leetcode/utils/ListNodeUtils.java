a244a2d724d5976aa6043fbc5084e75df559692b
Koray Balcioglu <balcioglu.koray@gmail.com>
2021-11-14 19:55:01 +0300

    ListNodeUtils

--- src/test/java/com/solutions/leetcode/utils/ListNodeUtils.java
+++ src/test/java/com/solutions/leetcode/utils/ListNodeUtils.java
@@ -75,4 +75,14 @@ public class ListNodeUtils {
         }
         return previous;
     }
+    public static int countListNode(ListNode head) {
+        if (head == null)
+            return 0;
+        int count = 1;
+        while (head.next != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
 }
