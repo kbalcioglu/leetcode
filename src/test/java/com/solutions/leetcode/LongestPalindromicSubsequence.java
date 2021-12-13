ff722481f435347170905cf65932c1b9a1b082b5
Koray Balcioglu <balcioglu.koray@gmail.com>
2021-12-13 17:31:10 +0300

    https://leetcode.com/problems/longest-palindromic-subsequence/

--- src/test/java/com/solutions/leetcode/LongestPalindromicSubsequence.java
+++ src/test/java/com/solutions/leetcode/LongestPalindromicSubsequence.java
@@ -3,10 +3,11 @@ package com.solutions.leetcode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+//https://leetcode.com/problems/longest-palindromic-subsequence/
 public class LongestPalindromicSubsequence {
 
     /*
-    https://leetcode.com/problems/longest-palindromic-subsequence/
+
     Given a string s, find the longest palindromic subsequence's length in s.
     A subsequence is a sequence that can be derived from another sequence by deleting some or no elements
     without changing the order of the remaining elements.
@@ -17,14 +18,12 @@ public class LongestPalindromicSubsequence {
     }
 
     @Test
-    public void test_s1() {
+    public void test_1() {
         String s = "bbbab";
         int expected = 4;
 
-        int result = this.longestPalindromeSubseq_dp_topdown(s);
-        int result2 = this.longestPalindromeSubseq_dp_bottomup(s);
+        int result = this.longestPalindromeSubseq(s);
         this.assertion(expected, result);
-        this.assertion(expected, result2);
     }
 
     @Test
@@ -32,20 +31,18 @@ public class LongestPalindromicSubsequence {
         String s = "cbbd";
         int expected = 2;
 
-        int result = this.longestPalindromeSubseq_dp_topdown(s);
-        int result2 = this.longestPalindromeSubseq_dp_bottomup(s);
+        int result = this.longestPalindromeSubseq(s);
         this.assertion(expected, result);
-        this.assertion(expected, result2);
     }
 
-    public int longestPalindromeSubseq_dp_topdown(String s) {
+    public int longestPalindromeSubseq(String s) {
         int n = s.length() - 1;
         int dp[][] = new int[s.length() + 1][s.length() + 1];
 
         return longestPalindrome(dp, s, 0, n);
     }
 
-    public int longestPalindrome(int dp[][], String s, int i, int j) {
+    private int longestPalindrome(int dp[][], String s, int i, int j) {
         if (i > j) {
             return 0;
         } else if (i == j) {
@@ -60,30 +57,6 @@ public class LongestPalindromicSubsequence {
             dp[i][j] = Integer.max(left, right);
         }
         return dp[i][j];
-    }
-
-    // yanlış çalışıyor
-    public int longestPalindromeSubseq_dp_bottomup(String s) {
-        int n = s.length();
-        int dp[][] = new int[n][n];
-
-        for (int i = 0; i <= n; i++) {
-            dp[i][1] = 1;
-        }
-        for (int i = 2; i < n; i++) {
-            for (int j = 1; j < n - i + 1; j++) {
-                int k = j + i - 1;
-                if (s.charAt(j) == s.charAt(k)) {
-                    dp[j][i] = 2 + dp[j + 1][i - 2];
-                } else {
-                    int val1 = dp[j + 1][i - 1];
-                    int val2 = dp[j][i - 1];
-                    dp[j][i] = Math.max(val1, val2);
-                }
-            }
-        }
-
-        return dp[1][n-1];
     }
 }
 
